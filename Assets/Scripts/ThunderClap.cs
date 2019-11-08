558d9752ca80c6e9d65fc06e1d15e3d14d7bb473
bgustin53 <bgustin@srcs.org>
2019-11-08 14:27:28 -0800

    thunderclap code is there

---
+++ Assets/Scripts/ThunderClap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderClap : MonoBehaviour
+{
+    private bool canFlicker = true;
+    Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        light.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator Flicker()
+    {
+        if (canFlicker)
+        {
+            canFlicker = false;
+            light.enabled = true;
+            yield return new WaitForSeconds(10);
+            light.enabled = false;
+            yield return new WaitForSeconds(10);
+            canFlicker = true;
+        }
+        
+    }
+}
